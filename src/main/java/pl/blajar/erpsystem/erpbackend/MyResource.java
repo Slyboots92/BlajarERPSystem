02aef5913c462b80c0cd6bde22f1bb71c1e835a8
Slyboots92 <michal.blach92@gmail.com>
2015-08-17 03:03:50 +0200

    problem with deploy

--- src/main/java/pl/blajar/erpsystem/erpbackend/MyResource.java
+++ src/main/java/pl/blajar/erpsystem/erpbackend/MyResource.java
@@ -1,9 +1,17 @@
 package pl.blajar.erpsystem.erpbackend;
 
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.inject.Inject;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import pl.blajar.erpsystem.beans.ExampleBean;
+import pl.blajar.erpsystem.entity.Cargo;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -17,9 +25,14 @@ public class MyResource {
      *
      * @return String that will be returned as a text/plain response.
      */
+	//@EJB
+	//private ExampleBean exampleBean;
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+    	Cargo cargo = new Cargo();
+    	cargo.setId(1l);
+    	cargo.setName("wapno");
+        return "added";
     }
 }
